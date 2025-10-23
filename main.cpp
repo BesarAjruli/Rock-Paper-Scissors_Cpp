a005484685070e388be989b384a6796214361136
BesarAjruli <aquila1912@gmail.com>
2025-10-23 23:30:22 +0200

    Added logic to not accept invalid pics

--- main.cpp
+++ main.cpp
@@ -8,82 +8,86 @@ int main()
 
     Robot = rand() % 3;
 
-    cout << "Pick a sing using the number in front:\n0 - Rock\n1 - Paper\n2 - Scissors" << endl;
-    cin >> User;
-
-    if (User > 2 || User < 0)
+    while (true)
     {
-        cout << "Please pick a valid number!" << endl;
-        cin >> User;
-    }
-    else
-    {
-        switch (User)
-        /*
-        0 - Rock
-        1 - Paper
-        2 - Scissors
-        */
+        cout << "Pick a sing using the number in front:\n0 - Rock\n1 - Paper\n2 - Scissors" << endl;
+        if(cin >> User && (User >= 0 && User <= 2))
         {
-            // Rock
-        case 0:
-            if (Robot == 2)
-            {
-                cout << "You won!" << endl;
-                result = 1;
-            }
-            else if (Robot == 0)
-            {
-                cout << "It's a tie!" << endl;
-                result = -1;
-            }
-            else
-            {
-                cout << "You lost" << endl;
-                result = 0;
-            }
-            break;
-
-            // Paper
-        case 1:
-            if (Robot == 0)
-            {
-                cout << "You won!" << endl;
-                result = 1;
-            }
-            else if (Robot == 1)
-            {
-                cout << "It's a tie!" << endl;
-                result = -1;
-            }
-            else
-            {
-                cout << "You lost" << endl;
-                result = 0;
-            }
-            break;
-
-            // Scissors
-        case 2:
-            if (Robot == 1)
-            {
-                cout << "You won!" << endl;
-                result = 0;
-            }
-            else if (Robot == 0)
-            {
-                cout << "It's a tie!" << endl;
-                result = -1;
-            }
-            else
-            {
-                cout << "You lost" << endl;
-                result = 0;
-            }
-            break;
-        default:
             break;
         }
+        else
+        {
+            cout << "Please pick a valid number!" << endl;
+            cin.clear();
+            cin.ignore(10000, '\n');
+        }
+    }
+
+    switch (User)
+    /*
+    0 - Rock
+    1 - Paper
+    2 - Scissors
+    */
+    {
+        // Rock
+    case 0:
+        if (Robot == 2)
+        {
+            cout << "You won!" << endl;
+            result = 1;
+        }
+        else if (Robot == 0)
+        {
+            cout << "It's a tie!" << endl;
+            result = -1;
+        }
+        else
+        {
+            cout << "You lost" << endl;
+            result = 0;
+        }
+        break;
+
+        // Paper
+    case 1:
+        if (Robot == 0)
+        {
+            cout << "You won!" << endl;
+            result = 1;
+        }
+        else if (Robot == 1)
+        {
+            cout << "It's a tie!" << endl;
+            result = -1;
+        }
+        else
+        {
+            cout << "You lost" << endl;
+            result = 0;
+        }
+        break;
+
+        // Scissors
+    case 2:
+        if (Robot == 1)
+        {
+            cout << "You won!" << endl;
+            result = 0;
+        }
+        else if (Robot == 0)
+        {
+            cout << "It's a tie!" << endl;
+            result = -1;
+        }
+        else
+        {
+            cout << "You lost" << endl;
+            result = 0;
+        }
+        break;
+    default:
+        break;
     }
 
     return 0;
