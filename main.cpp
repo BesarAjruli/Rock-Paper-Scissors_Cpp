ff3cf18e897b137bf7eac7fe2bc482f1f54d6664
BesarAjruli <aquila1912@gmail.com>
2025-10-23 21:28:04 +0200

    Added Users dialog

--- main.cpp
+++ main.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
+#include <cstdlib>
 using namespace std;
 
-int main(){
+int main()
+{
+    int rock, paper, scissors, User, Robot;
+    rock = 0;
+    paper = 1;
+    scissors = 2;
+
+    int Robot = rand() % 3;
+
+    cout << "Pick a sing using the number in front:\n0 - Rock\n1 - Paper\n2 - Scissors" << endl;
 
     return 0;
 }
